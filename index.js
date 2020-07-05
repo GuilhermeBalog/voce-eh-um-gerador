ef3593fdac74d992114636e4e30cfb072e928276
Guilherme Balog Gardino <guilherme.gardino@usp.br>
2020-07-05 02:07:49 -0300

    Adiciona a busca de imagens

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
+require('dotenv/config')
+const { google } = require('googleapis')
+const customSearch = google.customsearch('v1')
+
 const names = require('./names')
 
-function start(){
-    names.forEach(nome => console.log(nome))
+async function start(){
+    const response = await customSearch.cse.list({
+        auth: process.env.GOOGLE_API_KEY,
+        cx: process.env.SEARCH_ENGINE_ID,
+        q: 'cat',
+        num: 10,
+        searchType: 'image'
+    })
+
+    const urls = response.data.items.map(item => item.link)
+
+    console.dir(urls, { depth: null })
+
 }
 
 start()
